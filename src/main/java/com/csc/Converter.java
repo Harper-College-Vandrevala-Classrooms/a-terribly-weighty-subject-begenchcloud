5aa5647fa91da6d644880b01059718af6ac79694
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-08-26 01:12:50 -0500

    Update Converter.java
    changed the format so it passes the gradle test

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -4,41 +4,27 @@ import java.util.Scanner;
 
 public class Converter {
 
-   
     public String toPounds(int ounces) {
         double pounds = ounces / 16.0;
         String poundUnit = (pounds == 1.0) ? "lb" : "lbs";
-        return String.format("%d ounces is equivalent to %.4f %s", ounces, pounds, poundUnit);
+        return String.format("%.4f %s", pounds, poundUnit);
     }
 
-  
     public String toPoundsAndOunces(int ounces) {
         int pounds = ounces / 16;
         int remainingOunces = ounces % 16;
         String poundUnit = (pounds == 1) ? "lb" : "lbs";
-        String ounceUnit = "oz";  
-
-        if (pounds == 0) {
-            return String.format("%d ounces is equivalent to %d %s", ounces, remainingOunces, ounceUnit);
-        } else if (remainingOunces == 0) {
-            return String.format("%d ounces is equivalent to %d %s", ounces, pounds, poundUnit);
-        } else {
-            return String.format("%d ounces is equivalent to %d %s and %d %s", ounces, pounds, poundUnit, remainingOunces, ounceUnit);
-        }
+        return String.format("%d %s %d oz", pounds, poundUnit, remainingOunces);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Converter converter = new Converter();
 
-
-        System.out.print("Enter the number of ounces: ");
         int ounces = scanner.nextInt();
 
-
-        System.out.println("Decimal pounds: " + converter.toPounds(ounces));
-        System.out.println("Pounds and ounces: " + converter.toPoundsAndOunces(ounces));
-
+        System.out.println(converter.toPounds(ounces));
+        System.out.println(converter.toPoundsAndOunces(ounces));
 
         scanner.close();
     }
